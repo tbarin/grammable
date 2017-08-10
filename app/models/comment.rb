53ca49b6b190feaf7009b472b5277661a6646242
Teresa Barin <tbarin@gmail.com>
2017-08-10 02:26:44 +0000

    removed test grams

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+
   belongs_to :gram
   belongs_to :user
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -18,14 +18,12 @@ RSpec.describe CommentsController, type: :controller do
     it "should require a user to be logged in to comment on a gram" do
       gram = FactoryGirl.create(:gram)
       post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
-
       expect(response).to redirect_to new_user_session_path
     end
 
     it "should return http status code of not found if the gram isn't found" do
       user = FactoryGirl.create(:user)
       sign_in user
-
       post :create, params: { gram_id: 'YOLOSWAG', comment: { message: 'awesome gram' } }
       expect(response).to have_http_status :not_found
 
